ba7fc11b5a659a55975189dab3f2136a22a6df33
Isha <isha01verma@gmail.com>
2026-07-09 12:14:00 +0530

    task model dir changed

--- backend/route/taskRoute.js
+++ backend/route/taskRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const taskController = require("../Controller/taskController");
+const taskController = require("../controller/taskController");
 const auth = require("../middleware/auth");
 
 router.post("/add", auth, taskController.addTask);
